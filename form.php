767a1daac24dd09a352fa56c15c6c8c39ee9013a
karpok123 <149933182+karpok123@users.noreply.github.com>
2023-11-18 06:29:25 +0700

    Update form.php

--- form.php
+++ form.php
@@ -1,16 +1,21 @@
 <?php
-define ('url',"*token tlg bot*");
+function kirimPesan() {
+  
+var $name = $.getElementById['name'];
 
-$name = $_GET['name'];
+var $message = $.getElementById['message'];
 
-$message = $_GET['message'];
+var $phone = $.getElementById['phone'];
 
-$phone = $_GET['phone'];
+var token = '6724446867:AAHaew2LwJP8LCdka9piAB8psfeokKZTRmc'; // Ganti dengan token bot yang kamu buat
+var grup = '6470863095'; // Ganti dengan chat id dari bot yang kamu buat
 
-$chat_id = '{your chat id}';
 
-$message = urlencode("Name:".$name."\nPhone: ".$phone."\nMessage: ".$message);
+var $message = gabungan("Name%3A%0A" + name.value + %0APhone%3A%0A + phone.value + %0AMessage3A%0A + message.value);
 
-file_get_contents(url."sendmessage?text=".$message."&chat_id=".$chat_id."&parse_mode=HTML");
+.ajax({
+  url: `https://api.telegram.org/bot${token}/sendMessage?chat_id=${grup}&text=${gabungan}&parse_mode=html`,
+  method: `POST`,
+  })
 
-?>
+  }
